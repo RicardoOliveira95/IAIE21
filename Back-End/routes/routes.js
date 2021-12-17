44efc0438defea471522858b01477d4145c43adb
Ricardo Oliveira <57198074+RicardoOliveira95@users.noreply.github.com>
2021-12-17 16:32:13 +0000

    Update routes.js

--- Back-End/routes/routes.js
+++ Back-End/routes/routes.js
@@ -44,5 +44,19 @@ router.get('/about',function(req,res){
 	res.sendFile(__dirname+'/FrontEnd/about.html');
 })
 
+router.delete('/eventos/:id',(req,res)=>{
+	const update=parseInt(req.params.id)
+	const query=bd.connection.query('DELETE FROM Evento WHERE idEvento=?',update,function(err,rows,fields){
+		console.log(query.sql)
+
+		if(!err){
+			console.log("Database updated successfully.")
+		}
+		else
+			console.log(err)
+	});
+	res.end();
+})
+
 module.exports=router;
 //app.get('/eventos',eventController.read);
