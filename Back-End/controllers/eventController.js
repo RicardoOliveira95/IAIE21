3f37254429e76d1da81f2f6a364c28d7cb4b61a9
Ricardo Oliveira <57198074+RicardoOliveira95@users.noreply.github.com>
2021-12-24 18:55:38 +0000

    Update eventController.js
    postar eventos para o moloni

--- Back-End/controllers/eventController.js
+++ Back-End/controllers/eventController.js
@@ -23,7 +23,22 @@ function post(req, res){
 	var data=req.body.data_inicio;
 	var desc=req.body.descricao;
 	var tipo=req.body.tipo;
-	  const post = [data, desc, tipo];
+	var preco=req.body.preco;
+	//moloni product [POST no MOLONI]
+	var product={"company_id":"0","category_id":"4655137","type":"1","name":`${desc}`,
+"summary":"","reference":"MA_26_","ean":"","price":`${preco}`,"unit_id":"1824561","has_stock":"0",
+"stock":"0","pos_favorite":"0","at_product_category":"","exemption_reason":"1",
+"taxes":{"tax_id":"2375396","value":"2.3","order":"3","cumulative":"0"},
+"suppliers":{"supplier_id":"","cost_price":""},"warehouses":{"warehouse_id":"","stock":""},"warehouse_id":""};
+
+	app.moloni.products("insert",product,function(error,result){
+		if(error)
+			return console.error(error);
+		
+		console.log(result);
+	})
+	
+	  const post = [data, desc, tipo, preco];
 	  const sql1 = bd.connection.query('INSERT INTO Evento SET data_inicio = ?,descricao = ?,tipo = ?',post,
 	  function (err, rows, fields) {
 		console.log(sql1.sql);
